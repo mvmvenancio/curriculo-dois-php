f164e4f34f0ae2b91ca0d38201f9c8a658da7905
Maicon Venâncio <mvmvenancio@gmail.com>
2025-08-16 22:01:40 -0300

    Currículo PHP Dois
    Criação de um Segundo Site Currículo PHP Dois

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php include 'includes/header.php'; ?>
+
+<main>
+     <br>
+    <section>
+      <h2><b>Resumo Profissional</b><br></h2>
+      <p>Analista Administrativo/Financeiro com mais de 20 anos de experiência em gestão de estoque, organização de documentos,
+        análise e compra de materiais, conciliação bancária, organização de contas a pagar e receber, atendimento ao cliente e
+        uso de tecnologias (CRM).
+        <br>Busco uma oportunidade de trabalho em uma empresa que valorize o meu potencial e me permita contribuir para o crescimento
+        da empresa na área tecnológica.
+      </p>
+    </section>
+    
+    <section>
+      <h2><b>Habilidades e Competências</b><br></h2>
+      <ul>
+      <li>Administração de estoque, com experiência em recebimento, armazenamento, organização e controle de estoques de materiais hospitalares. análise e compra de materiais, contato e negociação com fornecedor.</li>
+      <li>Organização de documentos, com experiência em organização de arquivos, documentos e patrimônio.</li>
+      <li>Atendimento ao cliente, com experiência em atendimento a clientes e fornecedores, presencialmente e por telefone.</li>
+      <li>Atendimento ao cliente, com experiência em atendimento a clientes e fornecedores, presencialmente e por telefone.</li>
+      </ul>
+    </section>
+  
+    <section>
+    <h2><b>Experiência</b><br></h2>
+        <p>Janeiro 2025 - Julho 2025 - SENAC<br>
+        Assistente de Suporte a Gestão:<br>
+        Principais atividades desempenhadas: Análise de materiais, contato e negociação com fornecedor, receber,
+        armazenar e organizar material adminstrativo, realizar inventário, separar e entregar material nos setores, controle de alvarás,
+        atualizar sistema, organizar patrimônio.</p>
+        <p>Novembro 2019 - Dezembro 2024 - HOSPITAL FLORIANÓPOLIS, IMAS<br>
+          Almoxarife:<br>
+          Principais atividades desempenhadas: Receber, Armazenar e Organizar material hospitalar,
+          Realizar Inventário, Separar e Entregar material nos setores, Anotar Entradas e Saídas e Atualizar
+          Sistema, Atender colaboradores via presencialmente, por telefone, email, whatsapp e organizar
+          patrimônio.</p>
+        <p>Agosto 2014 - Julho 2019 - FLORATTO COMÉRCIO DE FLORES E PRESENTES<br>
+          Auxiliar Administrativo:<br>
+          Principais atividades desempenhadas: Preenchimento de planilhas e documentos, Organização de
+          arquivos de documentos, receber e Organizar estoque de loja, Organizar contas a pagar/receber,
+          Realizar atendimento de Colaboradores e Fornecedores, presencialmente, telefone e por email e
+          whatsapp.</p>
+        <p>Fevereiro 2014 - Maio 2014 - Lugati IT Services LTDA EPP<br>
+          Auxiliar Desenvolvedor:<br>
+          Principais atividades desempenhadas: Desenvolvedor Junior .NET.</p>
+        <p>Setembro 2011 - Dezembro 2013 - ORSEGUPS<br>
+          Arquivista:<br>
+          Principais atividades desempenhadas: Atendimento de colaboradores presencialmente e clientes,
+          por telefone e por email, Arquivar documentos dos colaboradores, Organizar arquivo morto/vivo de
+          colaboradores.</p>
+        <p>Novembro 2005 - Junho 2011 - LOGIN LOGISTICA DE RECARGA LTDA<br>
+          Auxiliar Administrativo:<br>
+          Principais atividades desempenhadas: Atendimento a clientes e vendedores presencialmente por
+          telefone e email, Controle de estoque de Cartões telefônicos, Caixa, Pagamento de contas.</p>
+        <p>Março 2001 - Outubro 2002 - Casan<br>
+          Office-Boy:<br>
+          Principais atividades desempenhadas: Atendimento, Pagamento de Contas, Entregar Documentos
+          internos e externos, Organização de documentos e Auxílio ao Setor financeiro.</p>
+        </section>
+  
+        <section>
+        <h2><b>Formação</b><br></h2>
+        <p>2003 - Instituto Estadual de Educação Florianópolis<br>
+          Segundo Grau Completo</p>
+          <p>2025 - Senac - Curso Técnico Completo<br>
+              Técnico em Administração</p>
+          <h2><b>Cursos/Certificados e Aperfeiçoamento</b><br></h2>
+          <p>2025 - Data Science Academy<br>
+            <a href="../curriculo/pages/curriculo-data-science-inteligencia-artificial.php">1 - Fundamentos da Data Science e Inteligência Artificial<br></a>
+            <a href="../curriculo/pages/curriculo-fundamentos-engenharia-dados.php">2 - Fundamentos de Engenharia de Dados<br></a>
+            <a href="../curriculo/pages/curriculo-fundamentos-linguagem-python-analise-dados-data-science.php">3 - Fundamentos de Linguagem Python para Análise de Dados e Data Science<br></a>
+            <a href="../curriculo/pages/curriculo-power-bi-business-intelligence-data-science.php">4 - Microsoft Power BI, Business Intelligence e Data Science<br></a></p>
+          <p>2024 - Fundação Estudar<br>
+              <a href="../curriculo/pages/curriculo-ciencia-computacao.php">1 - Introdução a Ciência da Computação<br></a></p>
+          <p>2024 - Digital Innovation One<br>
+            <a href="../curriculo/pages/curriculo-engenharia-dados-python.php">1 - Engenharia de Dados com Python<br></a>
+            <a href="../curriculo/pages/curriculo-desenvolvimento-front-end-angular.php">2 - Desenvolvimento Front-End com Angular<br></a>
+            <a href="../curriculo/pages/curriculo-data-analytics-power-bi.php">3 - Data Analytics com Power BI<br></a>
+            <a href="../curriculo/pages/curriculo-introducao-ciencia-de-dados-python-sql.php">4 - Introdução a Ciência de Dados com Python, SQL e Power BI<br></a></p>
+          <p>2023 - IFRS - Online<br>
+            <a href="../curriculo/pages/curriculo-projetos-de-sistemas-web.php">1 - Projeto de Sistemas Web<br></a>
+            <a href="../curriculo/pages/curriculo-banco-de-dados-fundamentos.php">2 - Banco de Dados I: Fundamentos<br></a>
+            <a href="../curriculo/pages/curriculo-administracao-financeira.php">3 - Administração Financeira<br></a>
+            <a href="../curriculo/pages/curriculo-administracao-estrategica.php">4 - Administração Estratégica<br></a>
+            <a href="../curriculo/pages/curriculo-projetos-elaboracao-e-gestao.php">5 - Projetos: Elaboração e Gestão<br></a>
+            <a href="../curriculo/pages/curriculo-contabilidade-empresarial.php">6 - Contabilidade Empresarial<br></a>
+            <a href="../curriculo/pages/curriculo-administrador-de-banco-de-dados.php">7 - Administrador de Banco de Dados<br></a>
+            <a href="../curriculo/pages/curriculo-programador-web.php">8 - Programador Web<br></a></p>
+            <p>2023 - Soft Blue<br>
+              <a href="../curriculo/pages/curriculo-sql-completo.php">1 - Banco de Dados - Completo<br></a></p>
+            <p>2023 - Fundação Bradesco/Escola Virtual<br>
+              <a href="../curriculo/pages/curriculo-fundamentos-logica-da-programacao.php">1 - Fundamentos de Lógica de Programação<br></a>
+              <a href="../curriculo/pages/curriculo-introducao-power-bi.php">2 - Microsoft Power BI<br></a></p>
+            <p>2023 – Cisco Networking Academy<br>
+              <a href="../curriculo/pages/curriculo-fundamentos-javascript.php">1 - Fundamentos JavaScript<br></a>
+              <a href="../curriculo/pages/curriculo-fundamentos-python.php">2 - Fundamentos Python<br></a></p>
+            <p>2012 á 2013 - Voffice Global Code<br>
+                <a href="../curriculo/pages/curriculo-academia-do-programador.php">1 - Academia do Programador<br></a>
+              <a href="../curriculo/pages/curriculo-academia-java.php">2 - Academia Java<br></a></p>
+            <p>2004 - Senac Florianópolis<br>
+              <a href="../curriculo/pages/curriculo-programa-educacao-para-trabalho.php">1 - Orientações Para Primeiro Emprego<br></a>
+              <a href="../curriculo/pages/curriculo-assistente-administrativo.php">2 - Assistente Administrativo<br></a></p>
+            <p>2001 e 2002 - Sest Senat - Infosul<br>
+              <a href="../curriculo/pages/curriculo-informatica-basica.php">1 - Informática Básica<br></a>
+              <a href="../curriculo/pages/curriculo-informatica-com-auxiliar-administrativo.php">2 - Treinamento em Informática<br></a></p>
+        </section>
+            
+        <section>
+              <h2><b>Cursos em Andamento:</b><br></h2>
+        <p>IFSUL de Minas<br>
+          Técnico em Informática</p>
+        </section>
+</main>
+
+<?php include 'includes/footer.php'; ?>
---
+++ pages/curriculo-projetos-de-sistemas-web.php
@@ -0,0 +1,11 @@
+<?php include '../includes/header.php'; ?>
+
+<main>
+    <link rel="stylesheet" href="../css/style.css">
+     <section class="center-content">
+         <embed src="../imagem/projetoDeSistemasWeb.pdf" type="application/pdf" width="500" height="500" alt="Academia Do Programador" title="Academia Do Programador"><br>
+         <a href="../imagem/projetoDeSistemasWeb.pdf" download class="download-button"><i class="fawnload"></i> Baixar Certificado</a>
+    </section><br>
+</main>
+
+<?php include '../includes/footer.php'; ?>
